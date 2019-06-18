1f81d80c32044156e23266a6b323ee62c1f24964
AlekseyM73 <pro.invader73@gmail.com>
2019-06-18 15:56:21 +0400

    Start

---
+++ src/HtmlDownloader.java
@@ -0,0 +1,14 @@
+public class HtmlDownloader {
+
+    private String filepath;
+    private String url;
+
+    public HtmlDownloader(String filepath, String url) {
+        this.filepath = filepath;
+        this.url = url;
+    }
+
+    public boolean download (){
+        return false;
+    }
+}
---
+++ src/Manager.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Manager {
+
+    private String filePath;
+    private String url;
+    private BufferedReader bufferedReader;
+
+   public void start(){
+       bufferedReader = new BufferedReader
+               (new InputStreamReader(System.in));
+
+       try {
+           filePathRequest();
+
+       } catch (IOException i){
+
+       }
+
+       try {
+           urlRequest();
+       } catch (IOException i1){
+
+       } finally {
+           try {
+               bufferedReader.close();
+           } catch (IOException i2){
+               i2.printStackTrace();
+           }
+
+       }
+
+            HtmlDownloader htmlDownloader = new HtmlDownloader(filePath,url);
+            htmlDownloader.download();
+
+
+
+
+    }
+
+    private void filePathRequest() throws IOException{
+        System.out.println("Enter filepath");
+        filePath = bufferedReader.readLine();
+        if ("".equals(filePath)){
+            filePathRequest();
+        }
+
+    }
+    private void urlRequest() throws IOException {
+
+        System.out.println("Enter url");
+        url = bufferedReader.readLine();
+        if ("".equals(url)){
+            urlRequest();
+        }
+    }
+}
